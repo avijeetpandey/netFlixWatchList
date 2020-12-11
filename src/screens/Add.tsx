23562014bd0ef4de36feab60aa12db36ebf2b6a8
Avijeet Pandey <avijeet_official@outlook.com>
2020-12-11 23:12:26 +0530

    added Local Storage functionality

--- src/screens/Add.tsx
+++ src/screens/Add.tsx
@@ -5,9 +5,41 @@ import {Container, Form, Item, Input, Button, H1} from 'native-base';
 import shortId from 'shortid';
 import AsyncStorage from '@react-native-community/async-storage';
 
-const Add = () => {
+const Add = ({navigation}: any): JSX.Element => {
   const [name, setName] = useState('');
   const [totalSeason, setTotalSeason] = useState('');
+
+  const addToList = async () => {
+    try {
+      const seasonToWatch = {
+        id: shortId.generate(),
+        name,
+        totalSeason,
+        isWatched: false,
+      };
+
+      const storedValue: any = await AsyncStorage.getItem('@season_list');
+      const previousList = await JSON.parse(storedValue);
+
+      if (!previousList) {
+        const newList = [seasonToWatch];
+        await AsyncStorage.setItem('@season_list', JSON.stringify(newList));
+      } else {
+        previousList.push(seasonToWatch);
+        await AsyncStorage.setItem(
+          '@season_list',
+          JSON.stringify(previousList),
+        );
+      }
+
+      setName('');
+      setTotalSeason('');
+
+      navigation.navigate('Home');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -16,12 +48,22 @@ const Add = () => {
           <H1 style={styles.heading}>Add to watch List</H1>
           <Form>
             <Item rounded style={styles.formItem}>
-              <Input placeholder="Season Name" style={{color: '#eee'}} />
+              <Input
+                placeholder="Season Name"
+                style={{color: '#eee'}}
+                value={name}
+                onChangeText={(text) => setName(text)}
+              />
             </Item>
             <Item rounded style={styles.formItem}>
-              <Input placeholder="Total Seasons" style={{color: '#eee'}} />
+              <Input
+                placeholder="Total Seasons"
+                style={{color: '#eee'}}
+                value={totalSeason}
+                onChangeText={(text) => setTotalSeason(text)}
+              />
             </Item>
-            <Button rounded block>
+            <Button rounded block onPress={addToList}>
               <Text style={{color: '#eee'}}>Add</Text>
             </Button>
           </Form>
